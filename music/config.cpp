88a40cfdd25f19cd2cec69d080a4e91167d95623
Mr. Zorn <56258612+PulsarNeutronStar@users.noreply.github.com>
2023-09-17 20:40:32 +0200

    Initial commit

---
+++ music/config.cpp
@@ -0,0 +1,40 @@
+class CfgPatches
+{
+    class cvo_music
+    {
+        // Meta information for editor
+        name = "CVO Music";
+        author = "Overlord Zorn";
+        url = "http://chronivoron.net";
+
+        // Minimum compatible version. When the game's version is lower, pop-up warning will appear when launching the game.
+        requiredVersion = 2.0;
+
+        // Required addons, used for setting load order.
+        // When any of the addons is missing, pop-up warning will appear when launching the game.
+        requiredAddons[] = {};
+        
+        // List of objects (CfgVehicles classes) contained in the addon. Important also for Zeus content (units and groups)
+        units[] = {};
+
+        // List of weapons (CfgWeapons classes) contained in the addon.
+        weapons[] = {};
+    }
+};
+
+
+
+
+
+class CfgMusic
+{
+	#include "cvo_CfgMusicTracks.hpp"
+};
+
+class CfgMusicClasses
+{
+	class cvo_musicClass
+	{
+		displayName = "24th Chorni Voron Music";
+	};
+};
---
+++ music/cvo_CfgMusicTracks.hpp
@@ -0,0 +1,15 @@
+class cvo_music_chorniVoron
+{
+	name = "Chorni Voron";
+	sound[] = {"cvo_music\data\chorniVoron.ogg",db+0,1};
+	duration=339;
+	musicClass = "cvo_musicClass";
+};
+
+class cvo_music_papersPlease
+{
+	name = "Wolfenstein - Rocket Trains";
+	sound[] = {"cvo_music\data\rocketTrains.ogg",db+0,1};
+	duration=270;
+	musicClass = "cvo_musicClass";
+};
