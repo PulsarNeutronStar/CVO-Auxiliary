591370c8ffd6d056841cf348f18d74d52d66e1c6
Mr. Zorn <56258612+PulsarNeutronStar@users.noreply.github.com>
2023-09-21 13:35:46 +0200

    sub config.cpp

--- Addons/others/Config.cpp
+++ Addons/others/Config.cpp
@@ -28,34 +28,3 @@ class CfgPatches {
 	};
 
 };
-
-
-class CfgSounds
-{
-	sounds[] = {}; // OFP required it filled, now it can be empty or absent depending on the game's version
-
-	class cvo_healsound
-	{
-		name = "cvo_healsound";													// display name
-		sound[] = { "cvo_others\medical\medical_healsound.ogg", 1, 1, 100 };	// file, volume, pitch, maxDistance
-		titles[] = { 0, "[Healsound]" };										// subtitles
-
-		forceTitles = 0;			// Arma 3 - display titles even if global show titles option is off (1) or not (0)
-		titlesStructured = 0;		// Arma 3 - treat titles as Structured Text (1) or not (0)
-	};
-};
-
-
-class CfgFunctions
-{
-	class CVO
-	{
-		class Medical
-		{
-			class fullHeal 
-			{
-				file = "cvo_others\medical\fnc_fullHeal.sqf";  
-			};
-		};
-	}; 
-};
---
+++ Addons/others/medical/config.cpp
@@ -0,0 +1,43 @@
+class CfgPatches
+{
+	class CVO_Others_Medical
+	{
+		// Use meta information from specified addon. Used to avoid repeated declarations.
+		addonRootClass = "CVO_Others";
+
+		requiredVersion = 2.00;
+		requiredAddons[] = { "CVO_Others" };
+		units[] = {};
+		weapons[] = {};
+	};
+};
+
+class CfgSounds
+{
+	sounds[] = {}; // OFP required it filled, now it can be empty or absent depending on the game's version
+
+	class cvo_healsound
+	{
+		name = "cvo_healsound";															// display name
+		sound[] = { "cvo\auxiliary\others\medical\medical_healsound.ogg", 1, 1, 100 };	// file, volume, pitch, maxDistance
+		titles[] = { 0, "[Healsound]" };												// subtitles
+
+		forceTitles = 0;			// Arma 3 - display titles even if global show titles option is off (1) or not (0)
+		titlesStructured = 0;		// Arma 3 - treat titles as Structured Text (1) or not (0)
+	};
+};
+
+
+class CfgFunctions
+{
+	class CVO_Others            // Tag
+	{
+		class Medical           // Category
+		{
+			class fullHeal      // Function name -> Final Function will be: TAG_FNC_FunctionName
+			{
+				file = "cvo\auxiliary\others\medical\fnc_fullHeal.sqf";  
+			};
+		};
+	}; 
+};
