5b9bd8c16c4a4164815b1d4c39d8aa2f83a1bfe9
Mr. Zorn <56258612+PulsarNeutronStar@users.noreply.github.com>
2023-09-21 13:35:51 +0200

    Update mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -2,20 +2,20 @@ name = "CVO Auxiliary";
 
 author = "Overlord Zorn";
 
-picture         = "img\flag_voron_red_1024_2048.paa";              // Picture displayed from the expansions menu. Optimal size is 2048x1024
-hideName        = "false";                                          // Hide the extension name
-hidePicture     = "false";                                          // Hide the extension menu
+picture         = "img\flag_voron_red_1024_2048.paa";                                                                                               // Picture displayed from the expansions menu. Optimal size is 2048x1024
+hideName        = "false";                                                                                                                          // Hide the extension name
+hidePicture     = "false";                                                                                                                          // Hide the extension menu
 
-logoSmall       = "img\logo-voron-white_64.paa";                        // Display next to the item added by the mod 64x64
-logo            = "img\logo-voron-white_128.paa";                       // Logo displayed in the main menu 128x128
-logoOver        = "img\logo-voron-red_128.paa";                         // When the mouse is over, in the main menu 128x128
+logoSmall       = "img\logo-voron-white_64.paa";                                                                                                    // Display next to the item added by the mod 64x64
+logo            = "img\logo-voron-white_128.paa";                                                                                                   // Logo displayed in the main menu 128x128
+logoOver        = "img\logo-voron-red_128.paa";                                                                                                     // When the mouse is over, in the main menu 128x128
 
 tooltip         = "CVO Auxiliary";
-tooltipOwned    = "Who sold this to you?";                          // Tool tip displayed when the mouse is left over, in the main menu
-overview        = "24th Chorni Voron - Auxiliary Mod - Contains frequently used assets and Scripts used by the 24th Chorni Voron!";              // Overview text, displayed from the extension menu
-description     = "Contains frequently used assets and Scripts used by the 24th Chorni Voron!" // Probably in context with action
-action          = "http://discord.chornivoron.net/";                // Website URL, that can accessed from the expansions menu 
-actionName      = "Discord";                                    // label of button/tooltip in extension menu
+tooltipOwned    = "Who sold this to you?";                                                                                                          // Tool tip displayed when the mouse is left over, in the main menu
+overview        = "24th Chorni Voron - Auxiliary Mod - Contains frequently used assets and Scripts used by the 24th Chorni Voron!";                 // Overview text, displayed from the extension menu
+description     = "Contains frequently used assets and Scripts used by the 24th Chorni Voron!";                                                     // Probably in context with action
+action          = "http://discord.chornivoron.net/";                                                                                                // Website URL, that can accessed from the expansions menu 
+actionName      = "Discord";                                                                                                                        // label of button/tooltip in extension menu
 
 overviewPicture = "voron_round_olive_texture_1024.paa";
 overviewText = "24th Chorni Auxiliary Mod";
@@ -31,4 +31,3 @@ dlcColor[] =
     0.0,
     1
 };
-
