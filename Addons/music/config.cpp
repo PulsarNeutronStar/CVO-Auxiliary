a96cf8cd372b05cea3904b253340fe67242f54ee
Mr. Zorn <dennisAzorn@gmail.com>
2023-10-03 20:53:09 +0200

    Update config.cpp

--- Addons/music/config.cpp
+++ Addons/music/config.cpp
@@ -12,7 +12,7 @@ class CfgPatches
 
         // Required addons, used for setting load order.
         // When any of the addons is missing, pop-up warning will appear when launching the game.
-        requiredAddons[] = {};
+        requiredAddons[] = {"cba_common"};
         
         // List of objects (CfgVehicles classes) contained in the addon. Important also for Zeus content (units and groups)
         units[] = {};
