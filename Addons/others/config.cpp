79bf85d6d5e34c3ab3b7b75b670c59ddf0840c67
Mr. Zorn <dennisAzorn@gmail.com>
2023-10-16 16:08:32 +0200

    removed JND

--- Addons/others/config.cpp
+++ Addons/others/config.cpp
@@ -28,18 +28,14 @@ class CfgPatches {
 	};
 
 };
-
+/*
 class CfgFunctions
 {
 	class CVO_Others            // Tag
 	{
 		class Others           // Category
 		{
-			class JND_debugv2      // Function name -> Final Function will be: TAG_FNC_FunctionName
-			{
-				file = "z\cvo_aux\addons\others\fn_JND_debugv2.sqf";
-                postInit = 1;       // force call the function upon mission start, *after*  objects areinitialized
-			};
 		};
 	}; 
 };
+*/
